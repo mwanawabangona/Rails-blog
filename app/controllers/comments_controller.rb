e729b598787a7f8aed21cfa4de4de6c829030fbd
mwanawabangona <ellisallday@gmail.com>
2022-04-01 18:10:38 +0200

    Fix bug

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,20 +1,19 @@
 class CommentsController < ApplicationController
-    before_action :current_user, only: [:create]
-  
-    def create
-      @comment = current_user.comments.new(comments_params)
-      @comment.users_id = current_user.id
-      @comment.post_id = params[:post_id]
-  
-      if @comment.save
-        redirect_to user_post_path(current_user.id, Post.find(params[:post_id]))
-      else
-        render :new
-      end
-    end
-  
-    def comments_params
-      params.require(:comment).permit(:Text)
+  before_action :current_user, only: [:create]
+
+  def create
+    @comment = current_user.comments.new(comments_params)
+    @comment.users_id = current_user.id
+    @comment.post_id = params[:post_id]
+
+    if @comment.save
+      redirect_to user_post_path(current_user.id, Post.find(params[:post_id]))
+    else
+      render :new
     end
   end
-  
+
+  def comments_params
+    params.require(:comment).permit(:Text)
+  end
+end
--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,14 +1,13 @@
 class LikesController < ApplicationController
-    before_action :current_user, only: [:create]
-  
-    def create
-      @post = Post.find(params[:post_id])
-      new_like = current_user.likes.new(
-        users_id: current_user.id,
-        post_id: @post.id
-      )
-  
-      redirect_to "/users/#{@post.users_id}/posts/#{@post.id}", notice: 'Like created' if new_like.save
-    end
+  before_action :current_user, only: [:create]
+
+  def create
+    @post = Post.find(params[:post_id])
+    new_like = current_user.likes.new(
+      users_id: current_user.id,
+      post_id: @post.id
+    )
+
+    redirect_to "/users/#{@post.users_id}/posts/#{@post.id}", notice: 'Like created' if new_like.save
   end
-  
+end
