e84fd07d402035ecfdefb92c14cded7aaf948caf
mwanawabangona <ellisallday@gmail.com>
2022-04-01 15:34:47 +0200

    Fix bugs

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,3 +3,16 @@ class PostsController < ApplicationController
 
   def show; end
 end
+class PostsController < ApplicationController
+    def index
+      @user = User.find(params[:user_id])
+      @posts = @user.posts
+    end
+  
+    def show
+      @post = Post.find(params[:id])
+      @user = User.find(@post.users_id)
+      @comments = @post.comments
+    end
+  end
+  
