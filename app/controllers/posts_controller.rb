add4ae6558fd5d75f9caa13799647a9d4765b169
mwanawabangona <ellisallday@gmail.com>
2022-04-01 17:46:51 +0200

    Add posts controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,6 @@
 class PostsController < ApplicationController
+  before_action :current_user, only: [:create]
+
   def index
     @user = User.find(params[:user_id])
     @posts = @user.posts
@@ -8,5 +10,26 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
     @user = User.find(@post.users_id)
     @comments = @post.comments
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = current_user.posts.new(post_params)
+    @post.users_id = current_user.id
+
+    if @post.save
+      redirect_to user_path(current_user.id)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:Title, :Text)
   end
 end
