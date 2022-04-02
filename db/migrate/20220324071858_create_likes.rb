fc1ddc2be28db84cf9d0c241ab588bbb047ce5e3
mwanawabangona <ellisallday@gmail.com>
2022-04-02 07:56:35 +0200

    fix bugs

--- db/migrate/20220324071858_create_likes.rb
+++ db/migrate/20220324071858_create_likes.rb
@@ -2,7 +2,7 @@ class CreateLikes < ActiveRecord::Migration[7.0]
   def change
     create_table :likes do |t|
       t.references :users, null: false, foriegn_key: true, index:true
-      t.references :posts, null: false, foriegn_key: true, index:true
+      
 
       t.timestamps
     end
