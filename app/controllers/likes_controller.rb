9d7a39ce77ea6b4147b087d9a8a96746dd22450b
mwanawabangona <ellisallday@gmail.com>
2022-04-02 07:55:47 +0200

    update likes

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -2,12 +2,12 @@ class LikesController < ApplicationController
   before_action :current_user, only: [:create]
 
   def create
-    @post = Post.find(params[:post_id])
-    new_like = current_user.likes.new(
-      users_id: current_user.id,
-      post_id: @post.id
-    )
-
-    redirect_to "/users/#{@post.users_id}/posts/#{@post.id}", notice: 'Like created' if new_like.save
+   @post = Post.find(params[:post_id])
+    @like = current_user.likes.new(post_id: @post.id, users_id: current_user.id)
+    if @like.save
+      redirect_to user_posts_path(current_user.id, @post)
+    else
+      render :new
+    end
   end
 end
