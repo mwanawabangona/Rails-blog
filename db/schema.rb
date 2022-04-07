fcc9c15f86cbea21d16381c4fb49ac5c4bc9fd2d
mwanawabangona <ellisallday@gmail.com>
2022-04-07 16:31:03 +0200

    Fix bugs

--- db/schema.rb
+++ db/schema.rb
@@ -10,21 +10,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_04_02_052622) do
+ActiveRecord::Schema[7.0].define(version: 2022_04_05_150733) do
   create_table "comments", force: :cascade do |t|
     t.integer "users_id", null: false
+    t.integer "posts_id", null: false
     t.text "Text"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "post_id", null: false
+    t.index ["posts_id"], name: "index_comments_on_posts_id"
     t.index ["users_id"], name: "index_comments_on_users_id"
   end
 
   create_table "likes", force: :cascade do |t|
     t.integer "users_id", null: false
+    t.integer "posts_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "post_id", null: false
+    t.index ["posts_id"], name: "index_likes_on_posts_id"
     t.index ["users_id"], name: "index_likes_on_users_id"
   end
 
@@ -46,6 +50,18 @@ ActiveRecord::Schema[7.0].define(version: 2022_04_02_052622) do
     t.integer "Posts_Counter", default: 0, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string "unconfirmed_email"
+    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
