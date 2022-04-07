2f7102c2ce86ad24623e51a85ae40b3ba3431b2d
mwanawabangona <ellisallday@gmail.com>
2022-04-07 16:30:36 +0200

    Add user & post

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,10 +2,17 @@ class Post < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: 'users_id'
   has_many :likes
   has_many :comments
+  validates :Title, presence: true, length: { maximum: 250 }
+  validates :Likes_Counter, :Comments_Counter, numericality: { greater_than_or_equal_to: 0 }
 
   after_create :update_posts_counter
-
   after_destroy :update_posts_counter_after_destroy
+
+  def recent_comments
+    comments.order('created_at Desc').limit(5)
+  end
+
+  private
 
   def update_posts_counter
     author.increment!(:Posts_Counter)
@@ -13,9 +20,5 @@ class Post < ApplicationRecord
 
   def update_posts_counter_after_destroy
     author.decrement!(:Posts_Counter)
-  end
-
-  def recent_comments
-    comments.order('created_at Desc').limit(5)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,13 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable, :confirmable
   has_many :posts, foreign_key: 'users_id'
   has_many :comments, foreign_key: 'users_id'
   has_many :likes, foreign_key: 'users_id'
+  validates :Name, presence: true
+  validates :Posts_Counter, numericality: { greater_than_or_equal_to: 0 }
 
   def recent_posts
     posts.order('created_at DESC').limit(3)
